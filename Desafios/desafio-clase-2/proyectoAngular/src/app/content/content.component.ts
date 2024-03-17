45c42ff1d3ccec52de3384fbb876cdc6a987268e
Yasmin Burrut <nawsper@gmail.com>
2024-03-16 22:55:46 -0300

    Se crean los componenetes toolbar, menu y content

---
+++ Desafios/desafio-clase-2/proyectoAngular/src/app/content/content.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrl: '../app.component.scss',
+})
+export class ContentComponent {}
