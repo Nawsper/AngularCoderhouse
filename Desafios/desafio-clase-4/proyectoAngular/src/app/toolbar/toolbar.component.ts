1a83bb4840a9be62f52e34cec2899d4b45cc4051
Yasmin Burrut <nawsper@gmail.com>
2024-03-21 22:51:21 -0300

    Se crea desafio de la clase 4

---
+++ Desafios/desafio-clase-4/proyectoAngular/src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrl: '../app.component.scss',
+})
+export class ToolbarComponent {}
